27acd490a94fe100f11eb1de638125f29b94ba86
Gouri saini <gourisaini241@gmail.com>
2023-08-05 15:40:44 +0530

    gsap

--- script.js
+++ script.js
@@ -8,70 +8,96 @@ $(".slide-box").slick({
 var tl = gsap.timeline();
 
 tl
-    .from("main header",{
+    .from("main header", {
         x: "-100%",
         opacity: 0,
     })
 
-    .from("section",{
-        x:"-100%",
-        opacity:0
-    })
-
-    
-    .from("aside",{
-        x:"-100%",
-        opacity:0
-    })
-
-    .from("#sideline",{
-        x:"-100%",
-        opacity:0
-    })
-
-    gsap.from("#second>img",{
-        scrollTrigger:{
-            trigger: "#second>img",
-            scroller:"body",
-            // markers: true,
-            start:"top 60%",
-            end:"top 10%",
-            scrub:5,
-        },
-        x: "-20%",
-        opacity:0,
-        // rotate: 360,
-        duration:2
-    })
-
-    gsap.from("#second>div",{
-        scrollTrigger:{
-            trigger: "#second>div",
-            scroller:"body",
-            // markers: true,
-            start:"top 60%",
-            end:"top 60%",
-            scrub:5,
-        },
-        x: "20%",
-        opacity:0,
-        // rotate: 360,
-        duration:1
+    .from("section", {
+        x: "-100%",
+        opacity: 0
     })
 
 
-    gsap.from("#toppick .first-pick",{
-        scrollTrigger:{
-            trigger: "#topPick .first-pick",
-            scroller:"body",
-            // markers: true,
-            start:"top 60%",
-            end:"top 60%",
-            scrub:5,
-        },
-        y: 100,
-       opacity:0,
-    //   duration:1,
-      stagger: 0.1,
+    .from("aside", {
+        x: "-100%",
+        opacity: 0
+    })
 
+    .from("#sideline", {
+        x: "-100%",
+        opacity: 0
+    })
+
+gsap.from("#second>img", {
+    scrollTrigger: {
+        trigger: "#second>img",
+        scroller: "body",
+        start: "top 60%",
+        end: "top 10%",
+        scrub: 5,
+    },
+    x: "-20%",
+    opacity: 0,
+    duration: 2
+})
+
+gsap.from("#second>div", {
+    scrollTrigger: {
+        trigger: "#second>div",
+        scroller: "body",
+        start: "top 60%",
+        end: "top 60%",
+        scrub: 5,
+    },
+    x: "20%",
+    opacity: 0,
+    duration: 1
+})
+
+
+gsap.to(".first-pick", {
+    scrollTrigger: {
+        trigger: ".first-pick",
+        scroller: "body",
+        start: "top 90%",
+        end: "top 0%",
+        scrub: 5,
+    },
+    opacity: 1,
+})
+
+gsap.to(".boxes", {
+    scrollTrigger: {
+        trigger: ".boxes",
+        scroller: "body",
+        start: "top 90%",
+        end: "top 0%",
+        scrub: 5,
+    },
+    opacity: 1,
+})
+
+gsap.to("#service", {
+    scrollTrigger: {
+        trigger: "#service",
+        scroller: "body",
+        start: "top 90%",
+        end: "top 0%",
+        scrub: 5,
+    },
+    opacity: 1,
+})
+
+
+gsap.from("#footerIMG", {
+    scrollTrigger: {
+        trigger: "#footerIMG",
+        scroller: "body",
+        start: "top 90%",
+        end: "top 90%",
+        scrub: 5,
+    },
+    x: '-100%',
+    opacity: 0,
 })
